78e9870155510e2b5585203982a5676470dd1351
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-22 17:26:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 # code your #valid_move? method here
 
+def valid_move(array, index)
+  if index <= array
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
