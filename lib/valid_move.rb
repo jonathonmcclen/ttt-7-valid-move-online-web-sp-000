9bbedd54b0fe9ec9a1b105036ab0d41a711cd752
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-22 19:53:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?(array, index)
-  if index <= 8
-    position_taken
+  if index <= 8 && position_taken
+    true
   else
     false
   end
@@ -14,7 +14,7 @@ def position_taken?(array, i)
   
   if array[i] == " " || array[i] == "" || array[i] == nil
     false
-  elsif array[i] == "X" || "O"
+  elsif array[i] == "X" || array[i] == "O"
     true
   end
 end
